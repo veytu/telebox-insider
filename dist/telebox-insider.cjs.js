3e6a4e80079a5616797d2cece0d8a9b9816e6e49
xxxcc <xxxcc123@126.com>
2025-04-02 11:25:25 +0800

    update

--- dist/telebox-insider.cjs.js
+++ dist/telebox-insider.cjs.js
@@ -2074,6 +2074,7 @@ class TeleBox {
       ([coord, size, minimized, containerRect, collectorRect]) => {
         const absoluteWidth = size.width * containerRect.width;
         const absoluteHeight = size.height * containerRect.height;
+        console.log("rect change", size, containerRect, collectorRect);
         return {
           width: absoluteWidth + (minimized && collectorRect ? 1 : 0),
           height: absoluteHeight + (minimized && collectorRect ? 1 : 0),
